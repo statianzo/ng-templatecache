0f6b17111ed6d10c2bc7b585559d16fa4926bae9
Jason Staten <jstaten07@gmail.com>
2015-01-15 20:21:37 -0700

    beginnings

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import escape from 'js-string-escape';
+import {format} from 'util';
+import {extend} from 'lodash';
+
+const moduleOpenFormat = 'angular.module("%s"%s).run(["$templateCache", function($templateCache) {';
+const entryFormat = '$templateCache.put("%s", "%s");';
+const moduleCloseFormat = '}]);';
+
+const moduleOpen = format.bind(null, moduleOpenFormat);
+const entry = format.bind(null, entryFormat);
+const moduleClose = format.bind(null, moduleCloseFormat);
+const defaultOpts = {
+  module: 'templates',
+  standalone: false
+};
+
+function wrapWithModule(src, module, standalone) {
+  var secondArg = standalone ? ',[]' : '';
+  return [
+    moduleOpen(module, secondArg),
+    src,
+    moduleClose()
+  ].join('\n');
+}
+
+export default function render(opts) {
+  const {src, srcPath, module, standalone} = extend({}, defaultOpts, opts);
+  var expression = entry(srcPath, escape(src));
+
+  if (module) {
+    return wrapWithModule(expression, module, standalone);
+  }
+  else {
+    return expression;
+  }
+}
---
+++ test/templatecache.js
@@ -0,0 +1,22 @@
+import templatecache from '../src';
+
+export const withDefaults = {
+  setUp(done) {
+    this.template = '<div>Hello, {{firstName}}</div>';
+    this.result = templatecache({src: this.template, srcPath: 'templates/greet'});
+    done();
+  },
+
+  putsTemplateInCache(test) {
+    test.ok(this.result.includes(this.template), 'Missing original template');
+    test.ok(this.result.includes('$templateCache.put'), 'Missing $templateCache.put');
+    test.done();
+  },
+
+  wrapsInModule(test) {
+    test.ok(this.result.includes('angular.module'), 'Missing angular.module');
+    test.ok(this.result.match(/\}\]\);$/), 'Missing module closing');
+    test.done();
+  }
+
+};
